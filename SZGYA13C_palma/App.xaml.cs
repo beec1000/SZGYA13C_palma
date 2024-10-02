6277f5ed9717817816ae419fe22d84938806a776
beec1000 <beec1000.minecraft@gmail.com>
2024-10-02 13:07:09 +0200

    Add project files.

---
+++ SZGYA13C_palma/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SZGYA13C_palma
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ SZGYA13C_palma/Desszert.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZGYA13C_palma
+{
+    internal class Desszert
+    {
+        public string Nev { get; set; }
+        public string Tipus { get; set; }
+        public bool Dijazott { get; set; }
+        public int Ar { get; set; }
+        public string Ertekesites { get; set; }
+
+        public Desszert (string nev, string tipusa, bool dijazott, int ar, string ertekesites)
+        {
+            Nev = nev;
+            Tipus = tipusa;
+            Dijazott = dijazott;
+            Ar = ar;
+            Ertekesites = ertekesites;
+        }
+
+        public static List<Desszert> FromFile(string path)
+        {
+            List<Desszert> desszertek = new List<Desszert> ();
+
+            string[] line = File.ReadAllLines(path);
+
+            foreach (var l in line)
+            {
+                string[] d = l.Split(';');
+
+                string Nev = d[0];
+                string Tipus = d[1];
+                bool Dijazott = bool.Parse(d[2]);
+                int Ar = int.Parse(d[3]);
+                string Ertekesites = d[4];
+
+                Desszert desszert = new Desszert(Nev, Tipus, Dijazott, Ar, Ertekesites);
+                desszertek.Add(desszert);
+            }
+            return desszertek;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nev}, {Tipus}, {Dijazott}, {Ar}, {Ertekesites}";
+        }
+
+    }
+}
